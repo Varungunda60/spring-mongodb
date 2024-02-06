abf05d673f84c55e207bd89d6a1a4f4470161bae
Varungunda60 <varungunda60@gmail.com>
2024-02-05 19:41:45 -0800

    getters setters added

--- src/main/java/com/varun/mongodbpractice/entity/Task.java
+++ src/main/java/com/varun/mongodbpractice/entity/Task.java
@@ -1,16 +1,12 @@
 package com.varun.mongodbpractice.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 
 @Document(collection = "task")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Task {
     @Id
     private String taskId;
@@ -18,4 +14,44 @@ public class Task {
     private int severity;
     private String assignee;
     private int storyPoint;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public int getStoryPoint() {
+        return storyPoint;
+    }
+
+    public void setStoryPoint(int storyPoint) {
+        this.storyPoint = storyPoint;
+    }
 }
