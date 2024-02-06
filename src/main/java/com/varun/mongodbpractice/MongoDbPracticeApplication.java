d3719a581861f2810038aaa815d7dc35bb495b08
Varungunda60 <varungunda60@gmail.com>
2024-02-05 18:35:59 -0800

    pushing gitognore

---
+++ src/main/java/com/varun/mongodbpractice/MongoDbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.varun.mongodbpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbPracticeApplication.class, args);
+	}
+
+}
